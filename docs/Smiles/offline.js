56222fb1621dac37e00403b4159c67c5ae39d17d
Paul Gestwicki <paul.gestwicki@gmail.com>
2019-08-02 20:05:00 -0400

    Initial commit

---
+++ docs/Smiles/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1564790087,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/smiles-sheet0.png",
+		"images/ninja-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"images/bomb-sheet0.png",
+		"images/particles.png",
+		"images/tree-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ docs/Space/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1564790120,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/ship-sheet0.png",
+		"images/bolt-sheet0.png",
+		"images/badship-sheet0.png",
+		"images/badbolt-sheet0.png",
+		"images/badbase-sheet0.png",
+		"images/shield-sheet0.png",
+		"images/particles.png",
+		"images/sprite-sheet0.png",
+		"images/astroid-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/battery-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
